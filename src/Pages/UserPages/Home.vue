5566096635dcc42f5e2a04979c991fac8830341f
Vladislav <75082673+Vladislav716@users.noreply.github.com>
2021-08-27 14:48:44 +0300

    galary array random changed

--- src/Pages/UserPages/Home.vue
+++ src/Pages/UserPages/Home.vue
@@ -817,6 +817,19 @@ export default {
           });
         });
     },
+    shuffle(array) {
+      console.log(array, "--------------");
+      var tmp,
+        current,
+        top = array.length;
+      if (top)
+        while (--top) {
+          current = Math.floor(Math.random() * (top + 1));
+          tmp = array[current];
+          array[current] = array[top];
+          array[top] = tmp;
+        }
+    },
   },
   mounted() {
     // let coreScript = document.createElement('script')
@@ -898,6 +911,7 @@ export default {
       ),
       "Product"
     );
+    this.shuffle(this.projects);
   },
 };
 </script>
